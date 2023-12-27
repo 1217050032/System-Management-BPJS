94730fa970a662953dac8914f6e5d71d1b3e5e48
Dea Lestari <94585987+1217050032@users.noreply.github.com>
2023-12-27 06:55:04 -0800

    Update connection.js

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try{
         // mongodb connection string
-        const con = await mongoose.connect(`mongodb+srv://admin:admin123@cluster0.gx0g6r4.mongodb.net/?retryWrites=true&w=majority`);
-           
+        const con = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })  
 
         console.log(`MongoDB connected : ${con.connection.host}`);
     }catch(err){
